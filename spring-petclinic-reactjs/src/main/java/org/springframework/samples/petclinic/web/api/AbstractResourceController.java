848ece8c542b58bc4630a718319817989ef95dfd
zhanghao168 <623716242@qq.com>
2018-06-04 20:41:20 +0800

    add petclinic-reactjs

---
+++ spring-petclinic-reactjs/src/main/java/org/springframework/samples/petclinic/web/api/AbstractResourceController.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.petclinic.web.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+@CrossOrigin //处理跨域请求
+public class AbstractResourceController {
+
+}
